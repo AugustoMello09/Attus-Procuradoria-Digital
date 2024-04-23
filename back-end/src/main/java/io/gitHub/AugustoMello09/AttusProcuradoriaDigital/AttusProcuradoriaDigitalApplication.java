1f2953a4b6389a574727277fa4ba43bfe230bc95
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-04-23 10:15:43 -0300

    feat : Projeto Criado

---
+++ back-end/src/main/java/io/gitHub/AugustoMello09/AttusProcuradoriaDigital/AttusProcuradoriaDigitalApplication.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.AttusProcuradoriaDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttusProcuradoriaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttusProcuradoriaDigitalApplication.class, args);
+	}
+
+}
